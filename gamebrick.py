415c20441f94d9094006c4f412b74a1ecb0a8d49
ibnatysalsabila <salsajpr01@gmail.com>
2024-11-26 11:07:31 +0700

    membuat class ball untuk membuat elemen utama dalam permainan yang bergerak, memantul dan lainnya

--- gamebrick.py
+++ gamebrick.py
@@ -18,3 +18,54 @@ class GameObject(object):
         # Menghapus objek dari canvas
         self.canvas.delete(self.item)
 
+
+# Kelas Ball mewarisi GameObject, merepresentasikan bola dalam permainan
+class Ball(GameObject):
+    def __init__(self, canvas, x, y):
+        # Inisialisasi bola dengan radius, arah, dan kecepatan
+        self.radius = 10
+        self.direction = [1, -1]  # Arah bola (x, y)
+        self.speed = 5  # Kecepatan bola
+        item = canvas.create_oval(
+            x - self.radius, y - self.radius,
+            x + self.radius, y + self.radius,
+            fill='#87CEEB',  # Warna bola
+            outline='#4682B4',  # Warna outline
+            width=2
+        )
+        super(Ball, self).__init__(canvas, item)
+
+    def update(self):
+        # Mengupdate posisi bola dan memantul jika menyentuh tepi canvas
+        coords = self.get_position()
+        width = self.canvas.winfo_width()
+        if coords[0] <= 0 or coords[2] >= width:  # Pantulan horizontal
+            self.direction[0] *= -1
+        if coords[1] <= 0:  # Pantulan vertikal atas
+            self.direction[1] *= -1
+        x = self.direction[0] * self.speed
+        y = self.direction[1] * self.speed
+        self.move(x, y)
+
+    def collide(self, game_objects):
+        # Mengatur logika tabrakan bola dengan objek lain
+        coords = self.get_position()
+        x = (coords[0] + coords[2]) * 0.5  # Posisi tengah bola
+        if len(game_objects) > 1:
+            self.direction[1] *= -1  # Pantulan vertikal
+        elif len(game_objects) == 1:
+            game_object = game_objects[0]
+            coords = game_object.get_position()
+            # Menentukan arah pantulan berdasarkan posisi tabrakan
+            if x > coords[2]:
+                self.direction[0] = 1
+            elif x < coords[0]:
+                self.direction[0] = -1
+            else:
+                self.direction[1] *= -1
+
+        for game_object in game_objects:
+            if isinstance(game_object, Brick):
+                game_object.hit()  # Hit pada brick
+
+
